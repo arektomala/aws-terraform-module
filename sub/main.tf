29338d2a6ad1fde8adccc77d1cb60e41802feeb3
bombel415 <bombel415@gmail.com>
2021-11-07 23:58:43 +0100

    add

--- sub/main.tf
+++ sub/main.tf
@@ -1,18 +1,15 @@
 resource "aws_subnet" "subnet" {
-  for_each = var.subnet
-
-
-  availability_zone = each.value.availability_zone
-  availability_zone_id = each.value.availability_zone_id
-  cidr_block = each.value.cidr_block
-  customer_owned_ipv4_pool = each.value.customer_owned_ipv4_pool
-  ipv6_cidr_block = each.value.ipv6_cidr_block
-  map_customer_owned_ip_on_launch = each.value.map_customer_owned_ip_on_launch
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
-  outpost_arn = each.value.outpost_arn
-  assign_ipv6_address_on_creation = each.value.assign_ipv6_address_on_creation
-  vpc_id = each.value.vpc_id
-  tags = each.value.tags
+  availability_zone = var.availability_zone
+  availability_zone_id = var.availability_zone_id
+  cidr_block = var.cidr_block
+  customer_owned_ipv4_pool = var.customer_owned_ipv4_pool
+  ipv6_cidr_block = var.ipv6_cidr_block
+  map_customer_owned_ip_on_launch = var.map_customer_owned_ip_on_launch
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  outpost_arn = var.outpost_arn
+  assign_ipv6_address_on_creation = var.assign_ipv6_address_on_creation
+  vpc_id = var.vpc_id
+  tags = var.tags
 
   lifecycle {
     create_before_destroy = true
