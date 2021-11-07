df4bbc8b836453808b4030f032a860235c8ba310
bombel415 <bombel415@gmail.com>
2021-11-07 22:28:16 +0100

    Update variable.tf

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,12 +1,24 @@
-variable "vpc" {
-  type = map(object({
-    cidr_block = string
-    instance_tenancy = string
-    enable_dns_support = bool
-    enable_dns_hostnames = bool
-    enable_classiclink = bool
-    enable_classiclink_dns_support = bool
-    assign_generated_ipv6_cidr_block = bool
-    tags = map(string)
-  }))
+variable "cidr_block" {
+  type = string
+}
+variable "instance_tenancy" {
+  type = string
+}
+variable "enable_dns_support" {
+  type = bool
+}
+variable "enable_dns_hostnames" {
+  type = bool
+}
+variable "enable_classiclink" {
+  type = bool
+}
+variable "enable_classiclink_dns_support" {
+  type = bool
+}
+variable "assign_generated_ipv6_cidr_block" {
+  type = bool
+}
+variable "tags" {
+  type = map(string)
 }
