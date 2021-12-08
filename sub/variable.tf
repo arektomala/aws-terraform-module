1d7f759c01d8a7f49f3f825dd0f3595d4ffb0660
Arkadiusz Tomala <40761404+arektomala@users.noreply.github.com>
2021-12-08 15:49:23 +0100

    Update variable.tf

--- sub/variable.tf
+++ sub/variable.tf
@@ -1,33 +1,37 @@
-variable "availability_zone" {
-  type = list(string)
-}
-variable "availability_zone_id" {
-  type = string
-}
-variable "cidr_block" {
-  type = string
-}
-variable "customer_owned_ipv4_pool" {
-  type = string
-}
-variable "ipv6_cidr_block" {
-  type = string
-}
-variable "map_customer_owned_ip_on_launch" {
-  type = bool
-}
-variable "map_public_ip_on_launch" {
-  type = bool
-}
-variable "outpost_arn" {
-  type = string
-}
-variable "assign_ipv6_address_on_creation" {
-  type = bool
-}
-variable "vpc_id" {
-  type = string
-}
-variable "tags" {
-  type = map(string)
+variable "subnet" {
+  description = "subnet"
+  type = map(object({
+    availability_zone = string
+    availability_zone_id = string
+    cidr_block = string
+    customer_owned_ipv4_pool = string
+    ipv6_cidr_block = string
+    map_customer_owned_ip_on_launch = bool
+    map_public_ip_on_launch = bool
+    outpost_arn = string
+    assign_ipv6_address_on_creation = bool
+    vpc_id = string
+    tags = map(string)
+  }))
+
+
+  default = {
+    priv = {
+      availability_zone = "defaultzone"
+      availability_zone_id = null
+      cidr_block = "defaultblock"
+      customer_owned_ipv4_pool = ""
+      ipv6_cidr_block = null
+      map_customer_owned_ip_on_launch = false
+      map_public_ip_on_launch = false
+      outpost_arn = ""
+      assign_ipv6_address_on_creation = false
+      vpc_id = "def vpc"
+      tags = {
+        "Environment" : "dev"
+      }
+
+
+    }
+  } 
 }
